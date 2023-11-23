3565d16a86ecc708b1f3b90e22a10b0ead518fec
Enrico Squillante <131673364+enricoschillaci@users.noreply.github.com>
2023-11-23 18:50:00 +0100

    Update Studenti.java
    Aggiunti metodi base + implementazione del main

--- Studenti.java
+++ Studenti.java
@@ -1,1 +1,35 @@
+public class Studenti {
+	//Attributi
+	private String nome;
+	private String cognome;
+	private String matricola;
+	private int voto;
+	private int i=0;
+	private Corsi vCorsiIscritto[];
+	
+	//Costruttori
+	public Studenti() {
+		this.nome="Sconosciuto";
+		this.cognome="Sconosciuto";
+		this.matricola="Sconosciuto";
+	}
+	public Studenti(String n, String c, String m) {
+		this.nome=n;
+		this.cognome=c;
+		this.matricola=m;
+	}
 
+	//Get/Set
+	public String getMatricola() {
+		return matricola;
+	}
+	public void setVoto(int voto) {
+		this.voto=voto;
+	}
+	
+	//Metodi
+	public void addCorsoAStud(Corsi c) {
+		this.vCorsiIscritto[i]=c;
+		i++;
+	}
+}
